051af61d161338014215731973f20577545a4ecd
Krish <krushna607@gmail.com>
2024-08-20 20:19:19 +0530

    last commit

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Cartitems from '../Components/CartItems/CartItems';
+import Cartitems from '../Components/CartItems/Cartitems';
 
  const Cart = () => {
   return (
